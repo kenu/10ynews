6ace85393c534cfba295056446b7c1f54b32bcba
kenu <kenu.heo@gmail.com>
2021-09-10 01:35:17 +0900

    CI/CD github actions #5

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '10년전 오늘' });
+  const news = {
+    today: new Date().toLocaleDateString()
+  }
+  res.render('index', { title: '10년전 오늘', news });
 });
 
 module.exports = router;
