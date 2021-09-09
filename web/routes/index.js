e12c0440cb41863921270d1b050ac778d496591e
kenu <kenu.heo@gmail.com>
2021-09-10 02:21:35 +0900

    10년전 오늘 날짜 검색 링크 #6

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+function lpad(number) {
+  return number > 9 ? String(number) : '0' + number;
+}
+function getToday() {
+  const date = new Date();
+  const year = date.getFullYear() - 10;
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const today = year + '-' + lpad(month) + '-' + lpad(day);
+  return today;
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const today = getToday();
+  const query = `%22%EB%93%B1%EB%A1%9D+%3A${today}%22`;
   const news = {
-    today: new Date().toLocaleDateString()
+    today,
+    query
   }
   res.render('index', { title: '10년전 오늘', news });
 });
